0b557f2e732425cdffa18895572ed72a4a9565c7
Stefan Zota <43382608+stefanzota08@users.noreply.github.com>
2020-11-04 11:50:40 +0200

    Tema 1 - Detalii despre website

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Plan your event</title>
+</head>
+<body>
+
+<h1>Plan your event</h1>
+<h3>Acest website va reprezeta o companie ce se ocupa cu organizarea de evenimente precum:</h3>
+	<li>Nunta</li>
+	<li>Cununie</li>
+	<li>Botez</li>
+	<li>Majorat</li>
+	<li>etc.</li>
+<h3>Pentru inceput clientul alege un restaurant din cele aflate in parteneriat cu compania noastra. In continuare trebuie aleasa o data din calendar in care restaurantul este disponibil.</h3>
+<h3>Mai mult decat atat, clientul poate alege dintre pachetele predefinite sau poate alege variata 'CUSTOM' in care va alege toate detaliile legate de evenimentul pe care doreste sa il organizeze. Detalii precum:</h3>
+
+	<li>Nr. invitati</li>
+	<li>Nr. persoane la masa</li>
+	<li>Forma meselor</li>
+	<li>Tipul ornamentelor</li>
+	<li>Panou foto</li>
+	<li>Locatia evenimentului</li>
+	<li>Nr. ospatari</li>
+	<li>Barman</li>
+	<li>etc.</li>
+<h3>Desigur ca varianta 'CUSTOM' va fi mai costisitoare dar ofera libertate deplina persoanelor care vor sa isi organizeze evenimentul exact asa cum au visat.</h3>
+<h3>In functie de optiunile alese si numarul optiunilor alese pretul va fluctua.</h3>
+<h3>Dupa ce toate detaliile sunt puse la punct, clientul completeaza formularul de date personale si continua prin achitarea avansului. Achitarea se poate face exclusiv online.</h3>
+</body>
+</html>
